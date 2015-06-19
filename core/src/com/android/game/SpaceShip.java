e1f185a1af43810ced5549b6ed05493d33657e60
Panu <panurb@gmail.com>
2015-06-20 00:31:17 +0300

    SpaceShip now goes to a set destination

--- core/src/com/android/game/SpaceShip.java
+++ core/src/com/android/game/SpaceShip.java
@@ -3,24 +3,38 @@ package com.android.game;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 
 public class SpaceShip extends GameObject implements Drawable, Updateable {
 	String imgPath = "img/spaceship.jpg";
 	Texture img;
-	int xPos = 200;
-	int yPos = 200;
+    float speed = 1;
+	Vector2 position = new Vector2(200, 200);
+    Vector2 velocity = Vector2.Zero;
+    Vector2 destination = new Vector2(400, 150);
 
 	@Override
 	public void draw(SpriteBatch batch, AssetManager assMan) {
 		if(assMan.update()) {
 			img = assMan.get(imgPath);
-			batch.draw(img, xPos, yPos);
+			batch.draw(img, position.x, position.y, 100, 100);
 		}
 	}
 
 	@Override
 	public void update() {
-		xPos++;
-		yPos++;
+        if(!position.epsilonEquals(destination, 0.5f)) {
+            Vector2 dir = destination.cpy().sub(position).nor();
+            velocity.set(dir.scl(speed));
+        } else {
+            velocity.setZero();
+        }
+
+        position.add(velocity);
 	}
+
+    public void setDestination(Vector2 destination) {
+        // Input pls
+        this.destination = destination;
+    }
 }
