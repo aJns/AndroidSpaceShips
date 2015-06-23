1ef9dedd70f83e908bc501def2cb378bd98f5c83
Panu <panurb@gmail.com>
2015-06-23 18:13:57 +0300

    Code reformat

--- core/src/com/android/game/Player.java
+++ core/src/com/android/game/Player.java
@@ -7,11 +7,12 @@ import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
-	ArrayList<SpaceShip> ships = new ArrayList<SpaceShip>();
+    ArrayList<SpaceShip> ships = new ArrayList<SpaceShip>();
 
-	public Player(TreeMap<String, String> assetMap,
-                AssetManager assMan) {
-        SpaceShip ship = new SpaceShip(new Vector2(200, 200), assetMap, assMan);
+    public Player(TreeMap<String, String> assetMap,
+                  AssetManager assMan) {
+        SpaceShip ship = new SpaceShip(new Vector2(200, 200), assetMap,
+                assMan);
         ship.setDestination(new Vector2(400, 200));
         this.ships.add(ship);
     }
@@ -21,7 +22,7 @@ public class Player {
     }
 
     public void input(Vector2 pos) {
-        for(SpaceShip s : ships) {
+        for (SpaceShip s : ships) {
             s.setDestination(pos);
         }
     }
